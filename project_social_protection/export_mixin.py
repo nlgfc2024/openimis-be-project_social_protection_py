c9333e7445db7fdd3a3a67eb9b7161ae552e18be
Kovahiva <kovahiwa@gmail.com>
2026-07-24 08:40:53 +0200

    Restore export_mixin.py and register projectHistoryExport resolver

--- project_social_protection/export_mixin.py
+++ project_social_protection/export_mixin.py
@@ -5,9 +5,8 @@ class ExportableProjectQueryMixin(ExportableQueryMixin):
     module_name = 'project_social_protection'
     object_type = 'Project'
     related_field = 'benefit_plan'
-    exportable_fields = ['project']
+    exportable_fields = ['project', 'project_history']
     export_patches = {
-        'project': {
-            'field_name': 'name',
-        },
+        'project': [],
+        'project_history': [],
     }
